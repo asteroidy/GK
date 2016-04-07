2987e3a43b3e2fc62193598d4f2607b4379ff3f4
ast <ast@ast.pl>
2016-04-07 22:11:45 +0200

    stworzenie projektu i dodanie generatora asteroid

---
+++ Asteroids/Assets/scene/scripts/Spawner.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var obj : GameObject[];
+var spawnMin : float = 1f;
+var spawnMax : float = 2f;
+
+function Start () {
+	Invoke ("Spawn", Random.Range (spawnMin, spawnMax));
+}
+
+function Spawn () {
+	Instantiate (obj [Random.Range (0, obj.GetLength(0))], transform.position, Quaternion.identity);
+	Invoke ("Spawn", Random.Range (spawnMin, spawnMax));
+}
