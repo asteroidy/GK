406b19c364a74f10819f247471c8ae790077d4bb
ast <ast@ast.pl>
2016-04-09 12:08:33 +0200

    dodane przewijane tło

---
+++ Asteroids/Assets/scene/scripts/ScrollBackground.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+
+function Update () {
+	var mr : MeshRenderer;
+	mr = GetComponent.<MeshRenderer>();
+
+	var material : Material = mr.material;
+	var offset : Vector2 = material.mainTextureOffset;
+	offset.y+=0.006;
+	material.mainTextureOffset = offset;
+}
