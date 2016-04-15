47a7925af4d68c7cbdf12daac987bf34282062b4
ast <ast@ast.pl>
2016-04-15 15:27:13 +0200

    ustawienie przegranej po kolizji z asteroidą i spadania asteroid ze stałą losowaną z przedziału prędkością, a nie z grawitacją jak wcześniej

---
+++ Asteroids/Assets/scene/scripts/Crash.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+function OnTriggerEnter(other : Collider){
+	if(other.tag == "Player"){
+		//doSomething or
+		Application.LoadLevel(2);
+	}
+}
---
+++ Asteroids/Assets/scene/scripts/FallingDown.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var speed : float;
+var spin : float = 10.0;
+
+var minSpeed : float = 9.0;
+var maxSpeed : float = 14.0;
+
+function Start () {
+	speed = Random.Range (minSpeed, maxSpeed);
+}
+
+function Update () {
+	transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+	transform.Rotate(Vector3.forward, spin * Time.deltaTime);
+}
